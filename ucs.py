ee2c6c901a6c009d1ec823850f98dc8c25438cbe
nako75 <73942801+nako75@users.noreply.github.com>
2020-11-11 22:39:39 +0900

    Add files via upload

---
+++ ucs.py
@@ -0,0 +1,60 @@
+#!/bin/bash
+#ClashLand Emulator 11x Mod
+#Server Android
+ 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 3
+clear
+figlet "Ultrapowa Server beta 8.xx - © UCS  0.7.0.0"|lolcat
+echo ""
+echo $red " ____ ___.__   __                                                  "
+echo $red "|    |   \  |_/  |_____________  ______   ______  _  _______       "
+echo $red "|    |   /  |\   __\_  __ \__  \ \____ \ /  _ \ \/ \/ /\__  \      "
+echo $red "|    |  /|  |_|  |  |  | \// __ \|  |_> >  <_> )     /  / __ \_   "
+echo $red "|______/ |____/__|  |__|  (____  /   __/ \____/ \/\_/  (____  /    "
+echo $red "                               \/|__|                       \/     "
+echo $cyan "|This program is made by the Ultrapowa Development Team.|"
+echo $yellow "|    Ultrapowa is not affiliated to \"Supercell, Oy\".  |"
+echo $cyan "|        This program is copyrighted worldwide.         ||"
+sleep 5
+echo $white "Connecting Mysql ........."
+sleep 10
+echo $white "Default pass none ........."
+sleep 10
+echo $cyan " insert username database :"
+read kartu
+echo $cyan " insert name database :"
+read anda
+echo "\033[0;32m]List Socks : "
+read socks
+echo $red "Loading 50%....."
+sleep 5
+echo $red "Loading 100%......."
+sleep 5
+echo $red "Successfully connecting to database $anda"
+echo $red "TCP Server is Listening to : 127.0.0.1:9339......"
+sleep 5
+echo "Check Client Version 7.200...."
+sleep 3
+echo "1%"
+sleep 3
+echo "35%"
+sleep 3
+echo "50%"
+echo "60%"
+sleep 3
+echo "80%"
+sleep 3
+echo "90%"
+sleep 3
+echo "100%"
+sleep 5
+echo "Wait a second......"
+sleep 1
+echo "Server Started! Let's play Clash of Clans.."
